a120f39fd5ceb2317269883f1d47b8f1a5892a28
TheCoolBlackCat <blackcat@boun.cr>
2016-07-26 14:30:21 +0100

    Initial file upload

---
+++ code_latest.js
@@ -0,0 +1,68 @@
+//Full Time START
+setInterval(tlochms, 1000);
+
+function tlochms() {
+    var d = new Date();
+    document.getElementById("t-loc-h:m:s").innerHTML=d.toLocaleTimeString();
+}
+tlochms(); //Intital Call
+
+//Full Time END
+
+
+
+
+//Date START
+setInterval(whatDate, 1000);
+function whatDate() {
+var date = new Date();  //Gets Date
+
+var day = date.getDate();   //Gets Day
+var d = String(day);    //Converts 'day' to String
+
+var month = date.getMonth();    //Gets Month (Number from 0-11)
+
+var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];     //Array of Month Names
+
+var mo = monthNames[month];  //Fetches current month from array
+
+var y = date.getFullYear(); //Gets Year
+
+finaleDate= d + " " + mo + " " + y; //Writes full date
+
+document.getElementById("date").innerHTML = finaleDate;  //Prints date in id="date"
+}
+whatDate(); //Intital Call
+//Date END
+
+
+
+/*
+
+setInterval(updateTime, 1000);
+function updateTime() {
+var sec = date.getSeconds();
+var min = date.getMinutes();
+var hr = date.getHours();
+
+var s = String(sec);
+
+if (hr<10) {    //IF Hour is 0-9, prefix a zero
+    var h = String("0" + hr)
+}   else { 
+    var h = String(hr);
+}
+
+if (min<10) {    //IF Hour is 0-9, prefix a zero
+    var mi = String("0" + min)
+}   else { 
+    var mi = String(min);
+}
+
+document.getElementById("demo").innerHTML =h + ":" + mi + ":" + s;  //Prints time in id="time"
+}
+
+
+var timeArray = [date.getSeconds(),date.getMinutes(),date.getHours()]
+
+*/
