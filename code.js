6c07dad7671a01a1c228339b34309d4f355c3483
Tim <git@timothyclark.co.uk>
2020-05-17 14:14:01 +0100

    Restructured code

--- code.js
+++ code.js
@@ -1,144 +1,119 @@
-//FUNCTIONAL CODE
-//Do not post code here that is not fully tested!
+function date() {
+	var now = new Date(),
+			d = arguments[0][0], m = arguments[0][1], y = arguments[0][2],
+			delim = arguments[1],
+			options = arguments[2],
+			months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+			date = [];
 
-//START init
-function theCall(){
-	tlochms();
-	ddtmofy();
-	
-}
-setInterval(theCall, 1000); //Calls everything every 1 sec (could be a problem when code expands due to reduced efficency)
-theCall();  //Intital Call
-//END init
+	if (d) {
+		var x = now.getDate();
+		if (d === 'p') x = (x < 10 ? String(0+x): x); // Prefix zeroes < 10
+		if (options && options.sup) { // st/nd/rd/th superscript
+			x += "<sup>";
+			if (x === 1 || x === 21 || x === 31) x += "st";
+			else if (x === 2 || x === 22) x += "nd";
+			else if (x === 3 || x === 23) x += "rd";
+			else x += "th";
+			x += "</sup>"
+		}
+		date.push(x);
+	}
 
-//START Full Time
-function tlochms() {
-    var d = new Date();
-    document.getElementById("t-loc-h:m:s").innerHTML=d.toLocaleTimeString();
-}
-//END Full Time
+	if (m) {
+		var x = now.getMonth();
+		if (m === 't') x = months[x]; // Text month
+		if (m === 'st') x = months[x].slice(0, 3); // Short text month
+		date.push(x);
+	}
 
-//START Date Day_TextMOnth_FullYear
-function ddtmofy() {
-var date = new Date();  //Gets Date
+	if (y) {
+		var x = now.getFullYear();
+		if (y === 's') x = String(x).slice(2); // Short year
+		date.push(x);
+	}
 
-var day = date.getDate();   //Gets Day
-var d = String(day);    //Converts 'day' to String
-
-var month = date.getMonth();    //Gets Month (Number from 0-11)
-
-var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];     //Array of Month Names
-
-var mo = monthNames[month];  //Fetches current month from array
-
-var y = date.getFullYear(); //Gets Year
-
-var finaleDate= d + " " + mo + " " + y; //Writes full date
-
-document.getElementById("d-d_tmo_fy").innerHTML = finaleDate;  //Prints date in id="date"
-}
-//END Date Day_TextMOnth_FullYear
-
-
-
-
-
-
-
-
-
-
-//TESTS
-//Everything below this comment is in development/testing - always commit with /* */ around it
-
-//Date w/th START --> STILL TESTING, SO COMMENTED OUT
-/*
-setInterval(whatDate, 1000);
-function whatDate() {
-var date = new Date();  //Gets Date
-
-var day = date.getDate();   //Gets Day
-var d = String(day);    //Converts 'day' to String
-
-var th = "<sup>th</sup>";
-
-
-		 if (d=1)     //IF day is 1st
-   			{th = "<sup>st</sup>";}
-else if (d=21)     //IF day is 21st
-    		{th = "<sup>st</sup>";}
-else if (d=31)    //IF day is 31st
-    		{th = "<sup>st</sup>";}
-
-else if (d=2)		//IF day is 2nd
-    {th = "<sup>nd</sup>";}
-else if (d=22)	//IF day is 22nd
-    {th = "<sup>nd</sup>";}
-
-else if (d=3)		//IF day is 3rd
-    {th = "<sup>rd</sup>";}
-else if (d=23)		//IF day is 23rd
-    {th = "<sup>rd</sup>";}
-else {th = "<sup>th</sup>"}
-
-
-
-
-var month = date.getMonth();    //Gets Month (Number from 0-11)
-
-var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];     //Array of Month Names
-
-var mo = monthNames[month];  //Fetches current month from array
-
-var y = date.getFullYear(); //Gets Year
-
-finaleDate= d + th + " " + mo + " " + y; //Writes full date
-
-document.getElementById("d-ds_tmo_fy").innerHTML = finaleDate;  //Prints date in id="date"
-}
-whatDate(); //Intital Call
-//Date w/th END
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* MISC
-
-setInterval(updateTime, 1000);
-function updateTime() {
-var sec = date.getSeconds();
-var min = date.getMinutes();
-var hr = date.getHours();
-
-var s = String(sec);
-
-if (hr<10) {    //IF Hour is 0-9, prefix a zero
-    var h = String("0" + hr)
-}   else {
-    var h = String(hr);
+	return date.join(delim);
 }
 
-if (min<10) {    //IF Hour is 0-9, prefix a zero
-    var mi = String("0" + min)
-}   else {
-    var mi = String(min);
+function time() {
+	var now = new Date(),
+			h = arguments[0][0], m = arguments[0][1], s = arguments[0][2],
+			ms = arguments[0][3],
+			delim = arguments[1],
+			options = arguments[2],
+			time = [];
+
+			if (h) {
+				var x = now.getHours();
+				if (h === 'p' && x < 10) x = 0+String(x);
+				time.push(x);
+			}
+
+			if (m) {
+				var x = now.getMinutes();
+				if (m === 'p' && x < 10) x = 0+String(x);
+				time.push(x);
+			}
+
+			if (s) {
+				var x = now.getSeconds();
+				if (s === 'p' && x < 10) x = 0+String(x);
+				time.push(x);
+			}
+
+			if (ms) { // Won't work...yet (called every second)
+				var x = now.getMilliseconds();
+				time.push(x);
+			}
+
+			return time.join(delim);
 }
 
-document.getElementById("demo").innerHTML =h + ":" + mi + ":" + s;  //Prints time in id="time"
+function formatDateTime (str) {
+
 }
 
+function insertDateTime (el, func) {
+	el.innerHTML = func();
+	setInterval(function () {
+		el.innerHTML = func();
+	}, 1000);
+}
 
-var timeArray = [date.getSeconds(),date.getMinutes(),date.getHours()]
+var tests = document.getElementsByClassName("test");
+insertDateTime(tests[0], date.bind(this, [1, 1, 1], '/')); // d/m/yyyy
+insertDateTime(tests[1], date.bind(this, [1, 1, 1], '-')); // d-m-yyyy
+insertDateTime(tests[2], date.bind(this, [1, 1, 's'], ' ')); // d m yy
+insertDateTime(tests[3], date.bind(this, [1, 't', 's'], ' ')); // d Month yy
+insertDateTime(tests[3], date.bind(this, [1, 't', 1], ' ', {sup: true})); // d^th Month yy
+insertDateTime(tests[4], time.bind(this, [1, 1, 1, 0], ':')); // h:m:s
+insertDateTime(tests[5], time.bind(this, [1, 1, 1, 1], ':')); // h:m:s:ms
+insertDateTime(tests[6], time.bind(this, ['p', 'p', 'p', 0], ':')); // hh:mm:ss
+insertDateTime(tests[6], time.bind(this, ['p', 'p', 'p', 0], ':', {tz: "utc"})); // hh:mm:ss
+// insertDateTime(tests[7], date.bind(this, [1])); // d
+insertDateTime(tests[8], date.bind(this, [0,1])); // m
+insertDateTime(tests[9], date.bind(this, [0,0,1])); // yyyy
+insertDateTime(tests[10], time.bind(this, ['p'])); // hh
+insertDateTime(tests[11], time.bind(this, [0,'p'])); // mm
+insertDateTime(tests[12], time.bind(this, [0,0,'p'])); // ss
 
-*/
+var ids = {
+	// Backward Compatability
+	"d-d_tmo_fy": date.bind(this, [1,'t',1], ' '),
+	"t-loc-h:m:s": time.bind(this, ['p', 'p', 'p', 0], ':'),
+
+	// Upgraded Versions
+	"DateTime-d_month_yyyy": date.bind(this, [1,'t',1], ' '),
+	"DateTime-h:m:s": time.bind(this, ['p', 'p', 'p', 0], ':'),
+
+	"DateTime-dd/mm/yyyy": date.bind(this, [1,1,1], '/'),
+	"DateTime-dd^_month_yyyy": date.bind(this, [1,'t',1], ' ', {sup: true}),
+	"DateTime-dd": date.bind(this, [1]),
+	"DateTime-DD": date.bind(this, ['p']),
+}
+
+for (var id in ids) {
+	var el = document.getElementById(id);
+	if (el) insertDateTime(el, ids[id]);
+}
